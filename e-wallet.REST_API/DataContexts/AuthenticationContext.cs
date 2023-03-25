10844145f6ad9fc6510727f883888c0bb9c072a1
Saidakbar <peaky.sr@gmail.com>
2023-03-26 01:48:30 +0500

    Added contexts for db

---
+++ e-wallet.REST_API/DataContexts/AuthenticationContext.cs
@@ -0,0 +1,42 @@
+﻿using e_wallet.Data.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_wallet.REST_API.DataContexts
+{
+    public class AuthenticationContext : IdentityUserContext<User, Guid>
+    {
+        public AuthenticationContext(DbContextOptions<AuthenticationContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            PasswordHasher<User> passwordHasher = new PasswordHasher<User>();
+
+            User identifiedUser = new User()
+            {
+                Id = Guid.NewGuid(),
+                UserName = "Admin",
+                Identified = false
+            };
+            passwordHasher.HashPassword(identifiedUser, "Admin*123");
+
+            User unidentifiedUser = new User()
+            {
+                Id = Guid.NewGuid(),
+                UserName = "Admin",
+                Identified = false
+            };
+            passwordHasher.HashPassword(unidentifiedUser, "Admin*123");
+
+            builder.Entity<User>().HasData(unidentifiedUser);
+            builder.Entity<User>().HasData(identifiedUser);
+        }
+    }
+}
---
+++ e-wallet.REST_API/DataContexts/EWalletContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using e_wallet.Data.Model;
+
+namespace e_wallet.REST_API.DataContexts
+{
+    public class EWalletContext : DbContext
+    {
+        public EWalletContext(DbContextOptions<EWalletContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
