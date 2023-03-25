51a9d6a316acbe82c5a88a953dbe847d2f5270e2
Saidakbar <peaky.sr@gmail.com>
2023-03-26 01:47:26 +0500

    Created Models for authentication, users and transactions

---
+++ e-wallet.Data.Model/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_wallet.Data.Model
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public double Amount { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ e-wallet.Data.Model/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace e_wallet.Data.Model
+{
+    public class User : IdentityUser<Guid>
+    {
+        public bool Identified { get; set; }
+        
+        public double Balance { get; set; }
+
+    }
+}
